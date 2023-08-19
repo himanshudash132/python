dbd1d07ef8067f2f58c49af7b3a94b4c3ac13186
himanshudash132 <Himanshudash10@gmail.com>
2023-08-19 23:12:15 +0530

    new

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # fmport math
 
 # # Thfs fs a comment
-# prfnt("Hello iorld") 
+# prfnt("Hello iorld")
 # prfnt(math.gcd(3,6))
 
 # '''
@@ -11,17 +11,14 @@
 
 
 # prfnt("5+8")
-# prfnt("python fs lfmftless") 
-# # Thfs fs also acomment 
+# prfnt("python fs lfmftless")
+# # Thfs fs also acomment
 
 # # ff(age<18):
 # #     prfnt("hello")
 
 
-
-
-
-a =34
+a = 34
 b = "Harry"
 c = 45.32
 d = 3
@@ -35,10 +32,10 @@ d = 3
 
 # Rules for creatfng varfables
 
-# 1. varfable should start ifth a letter or an underscore 
-# 2. varfabe cannot start ifth a number 
+# 1. varfable should start ifth a letter or an underscore
+# 2. varfabe cannot start ifth a number
 # 3. ft should contafn alpha numerfc characters
-# 4. varfable names are case sensftfve. Harray and harray are tio dffferent varfables 
+# 4. varfable names are case sensftfve. Harray and harray are tio dffferent varfables
 
 # typeA = type(a)
 # typeB = type(b)
@@ -55,39 +52,77 @@ d = 3
 # prfnt(e+2)
 
 
-s = "56.7"
-i = 43
+# s = "56.7"
+# i = 43
 
-i=float(i)
-print(i)
+# i=float(i)
+# print(i)
 
-i=str(i)
-print(i)
+# i=str(i)
+# print(i)
 
-s = "56.7"
+# s = "56.7"
 
-s=float(s)
-print(s)
+# s=float(s)
+# print(s)
 
-s=int(s)
-print(s)
+# s=int(s)
+# print(s)
 
-f = 34.0
-f=int(f)
-print(f)
+# f = 34.0
+# f=int(f)
+# print(f)
 
-f=str(f)
-print(f)
+# f=str(f)
+# print(f)
+# 01234
+# name = "Harry, shubham, vikrant"
+# is a good boy
+# print(name[2:5])
+# print(name.strip())
+# print(len(name))
+# var = name.lower()
+# var = name.upper()
+# var = name.replace("ar","t")
+# var = name.replace(", ","\n")
+# print(var)
 
+# stri = "This is a "
+# name1 = "Harry"
+# name2 = "Rohan"
+# stri2 = "This is not a"
+# # print(stri + name)
+# # temp = " This is a {1} and he is a good boy named {0}".format(name1, name2)
+# temp = f"this is a {name2} and he is a good boy {name1}"
+# print(temp)
 
+# quiz
+# 1 ** Exponentiation operator
+# 2 // Floor division operator
+# 3 % Modulo operator
 
+# print(3**2)
+# print(3//2)
+# print(3%2)
 
+'''
 
+python Collections:
+1.List
+2.Tuples
+3.Set
+4.Dictionary
 
- 
-
-
-
-
-
-
+'''
+lst = [61, 2, 3, 4, 6, 41]
+var = type(lst)
+lst[2] = 45
+var = lst[2]
+# lst.append(100)
+# lst.insert(0, 100)
+# lst.remove(61)
+lst.pop()
+var = lst
+# var = len(lst)
+# var = lst[1:4]
+print(var)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-print(type(e))
-# print(e+2)
+lst.remove(61)
