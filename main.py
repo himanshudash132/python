93a458c66151c487c5d11531f4641692fa74b53d
himanshudash132 <Himanshudash10@gmail.com>
2023-08-19 13:05:52 +0530

    add new

---
+++ main.py
@@ -0,0 +1,53 @@
+# import cv2
+# import math
+
+# # This is a comment
+# print("Hello world") 
+# print(math.gcd(3,6))
+
+# '''
+# This is a multi line comment
+# '''
+
+
+# print("5+8")
+# print("python is limitless") 
+# # This is also acomment 
+
+# # if(age<18):
+# #     print("hello")
+
+
+
+
+a =34
+b = "Harry"
+c = 45.32
+d = 3
+# print(a+d)
+# print(a-d)
+# print(a*d)
+# print(a/d)
+
+# wrong syntax
+# harray project = 45
+
+# Rules for creating variables
+
+# 1. variable should start with a letter or an underscore 
+# 2. variabe cannot start with a number 
+# 3. It should contain alpha numeric characters
+# 4. variable names are case sensitive. Harray and harray are two different variables 
+
+# typeA = type(a)
+# typeB = type(b)
+# print(typeA)
+
+e = 31
+e = 3.14
+print(type(e))
+print(e+2)
+
+
+
+
