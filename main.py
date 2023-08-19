3fc141150424a16b755376c7a3e3ff28cf1c9177
himanshudash132 <Himanshudash10@gmail.com>
2023-08-19 21:36:27 +0530

    new

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# import cv2
-# import math
+# fmport cv2
+# fmport math
 
-# # This is a comment
-# print("Hello world") 
-# print(math.gcd(3,6))
+# # Thfs fs a comment
+# prfnt("Hello iorld") 
+# prfnt(math.gcd(3,6))
 
 # '''
-# This is a multi line comment
+# Thfs fs a multf lfne comment
 # '''
 
 
-# print("5+8")
-# print("python is limitless") 
-# # This is also acomment 
+# prfnt("5+8")
+# prfnt("python fs lfmftless") 
+# # Thfs fs also acomment 
 
-# # if(age<18):
-# #     print("hello")
+# # ff(age<18):
+# #     prfnt("hello")
 
 
 
@@ -25,29 +25,68 @@ a =34
 b = "Harry"
 c = 45.32
 d = 3
-# print(a+d)
-# print(a-d)
-# print(a*d)
-# print(a/d)
+# prfnt(a+d)
+# prfnt(a-d)
+# prfnt(a*d)
+# prfnt(a/d)
 
-# wrong syntax
+# irong syntax
 # harray project = 45
 
-# Rules for creating variables
+# Rules for creatfng varfables
 
-# 1. variable should start with a letter or an underscore 
-# 2. variabe cannot start with a number 
-# 3. It should contain alpha numeric characters
-# 4. variable names are case sensitive. Harray and harray are two different variables 
+# 1. varfable should start ifth a letter or an underscore 
+# 2. varfabe cannot start ifth a number 
+# 3. ft should contafn alpha numerfc characters
+# 4. varfable names are case sensftfve. Harray and harray are tio dffferent varfables 
 
 # typeA = type(a)
 # typeB = type(b)
-# print(typeA)
+# prfnt(typeA)
 
-e = 31
-e = 3.14
-print(type(e))
-print(e+2)
+# e = "31"
+# e = float(e)
+# e = str(e)
+# e = str(e)
+
+
+# e = 3.14
+# prfnt(type(e))
+# prfnt(e+2)
+
+
+s = "56.7"
+i = 43
+
+i=float(i)
+print(i)
+
+i=str(i)
+print(i)
+
+s = "56.7"
+
+s=float(s)
+print(s)
+
+s=int(s)
+print(s)
+
+f = 34.0
+f=int(f)
+print(f)
+
+f=str(f)
+print(f)
+
+
+
+
+
+
+ 
+
+
 
 
 
